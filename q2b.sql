32b07199a5a1228c73116f9babd3957a51077109
jonathanle17 <jonathan.le@mrjaffesclass.com>
2026-04-26 16:40:41 -0700

    q2b fixed

--- q2b.sql
+++ q2b.sql
@@ -1,20 +1,28 @@
+WITH TargetCustomers AS (
+    SELECT DISTINCT b.cname
+    FROM borrower b
+    JOIN loan l ON b.lno = l.no
+    WHERE l.type = 'jumbo mortgage'
+)
+/* identifies customers who have borrowed a jumbo mortgage loan, creating a temporary table TargetCustomers with their names */
+
+DELETE FROM borrower
+WHERE cname IN (SELECT cname FROM TargetCustomers);
+/* removes borrower links from customers*/
+
 DELETE FROM borrower
 WHERE lno IN (
     SELECT no
     FROM loan
     WHERE type = 'jumbo mortgage'
 );
-/* deletes all borrowers (children) of jumbo mortgage loans */
+/* removes borrower links from the loans of type 'jumbo mortgage' */
 
-DELETE FROM customer
-WHERE name IN (
-    SELECT b.cname
-    FROM borrower b
-    JOIN loan l ON b.lno = l.no
-    WHERE l.type = 'jumbo mortgage'
-);
-/* deletes all customers (parents) of jumbo mortgage loans, which will also delete their borrowers due to cascade delete */
+DELETE FROM customer 
+WHERE name IN (SELECT cname FROM TargetCustomers);
+/* removes customers who have borrowed a jumbo mortgage loan */
 
 DELETE FROM loan
 WHERE type = 'jumbo mortgage';
-/* deletes all jumbo mortgage loans, which will also delete their borrowers due to cascade delete */
+/* removes all loans of type 'jumbo mortgage' */
+
