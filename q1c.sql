b581e9f65229cbe8780adf2f34cbec07229f6693
jonathanle17 <jonathan.le@mrjaffesclass.com>
2026-04-26 14:48:30 -0700

    q1c done

---
+++ q1c.sql
@@ -0,0 +1,22 @@
+SELECT l.type
+FROM loan l
+LEFT JOIN borrower b ON l.no = b.lno
+/* joins all loans to their borrower by loan number */
+GROUP BY l.type
+/* group all loans by distinct types with their respective counts */
+HAVING COUNT(b.cname) = (
+    SELECT MIN(borrowerCount) 
+    /* select loan type with least count of borrowers */
+    FROM (
+        SELECT COUNT(b2.cname) AS borrowerCount 
+        /* counts number of names from borrower table */
+        FROM loan l2
+        LEFT JOIN borrower b2 ON l2.no = b2.lno
+        /* joins all loans to their borrower by loan number */
+        /* left join still includes loans with no borrowers */
+        GROUP BY l2.type
+        /* group all loans by distinct types with their respective counts */
+    ) AS typeCounts
+)
+/* group all loans by type with their resective counts */
+/* select the loan type with least count MIN() */
